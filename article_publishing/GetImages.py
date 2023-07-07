9209de30ddea217c6345c4b1b42a0d7f647b0181
Akshay Anil Indrayani <akshay.raut211@gmail.com>
2023-07-07 09:13:17 +0530

    updated the Xpath

--- article_publishing/GetImages.py
+++ article_publishing/GetImages.py
@@ -37,6 +37,7 @@ def get_image_url(args):
     options = Options()
     options.add_argument('--headless=new')
     driver = webdriver.Chrome(options=options)
+    #driver = webdriver.Chrome()
     driver.get('https://www.google.com/imghp')
 
     # get the image source
@@ -56,7 +57,7 @@ def get_image_url(args):
     #time.sleep(10)
 
     try: 
-        imageBiggerSize = WebDriverWait(driver,20).until(EC.presence_of_element_located((By.XPATH,'//*[@id="Sva75c"]/div[2]/div/div[2]/div[2]/div[2]/c-wiz/div/div/div/div[3]/div[1]/a/img[1]')))
+        imageBiggerSize = WebDriverWait(driver,20).until(EC.presence_of_element_located((By.XPATH,'//*[@id="Sva75c"]/div[2]/div[2]/div[2]/div[2]/c-wiz/div/div/div/div[3]/div[1]/a/img[1]')))
     except Exception as e:
         print (e)
         img = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[2]/c-wiz/div[3]/div[1]/div/div/div/div/div[1]/div[1]/div[1]/a[1]/div[1]/img')))
@@ -89,6 +90,7 @@ def getImages(summary):
       
         search_text = searchTitle + " " + searchAuthor
         total_get_url_payload.append((search_text, i+1))
+
     with Pool(cpu_count()-2) as pool:
             urls_results = pool.map(get_image_url, total_get_url_payload)
 
