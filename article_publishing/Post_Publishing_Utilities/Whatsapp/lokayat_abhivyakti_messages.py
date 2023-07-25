337e4715d8cadb8f90b81fe0f526f0523fe39332
Akshay Anil Indrayani <akshay.raut211@gmail.com>
2023-07-25 09:41:16 +0530

    minor bug fix in ahivyakti whatsapp message generation

--- article_publishing/Post_Publishing_Utilities/Whatsapp/lokayat_abhivyakti_messages.py
+++ article_publishing/Post_Publishing_Utilities/Whatsapp/lokayat_abhivyakti_messages.py
@@ -33,7 +33,7 @@ def generate_abhivyakti_whatsapp_message(summary):
 
 	file = open("EnglishWhatsappMessagesWithAbhivyaktiFooter.txt","w",encoding="UTF-8")
 	for i in range(1,total_articles_count):
-		message_section_one += "⭕ *"+summary[i]['article_title'].strip()+"*\n\n"
+		message_section_one = "⭕ *"+summary[i]['article_title'].strip()+"*\n\n"
 		message_section_one += "✒️ "+summary[i]['article_author']+"\n\n_"+summary[i]['article_excerpt']+"_\n\n*Read full article:*\n"
 		message_section_one += summary[i]['article_url']+"\n\n"
 
